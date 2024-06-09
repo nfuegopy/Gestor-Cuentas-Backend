4432edfb56a8ebdc522d7f7634ccb9b9599db199
Antonio Barrios <93525722+nfuegopy@users.noreply.github.com>
2024-06-09 09:40:25 -0400

    Update app.module.ts
    agregado el hello world

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,19 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { GastoModule } from './gasto/gasto.module';
 import { IngresoModule } from './ingreso/ingreso.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [CategoriaModule, FrecuenciaModule, UsuarioModule, GastoModule, IngresoModule, AuthModule],
+  imports: [
+    CategoriaModule,
+    FrecuenciaModule,
+    UsuarioModule,
+    GastoModule,
+    IngresoModule,
+    AuthModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
