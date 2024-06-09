21508d9f38e8d786cb1c20185a00bb4de64e9bae
Antonio Barrios <93525722+nfuegopy@users.noreply.github.com>
2024-06-09 09:55:19 -0400

    Update app.module.ts
    modifacion en app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { CategoriaModule } from './categoria/categoria.module';
 import { FrecuenciaModule } from './frecuencia/frecuencia.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { GastoModule } from './gasto/gasto.module';
 import { IngresoModule } from './ingreso/ingreso.module';
 import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -15,9 +15,9 @@ import { AppService } from './app.service';
     UsuarioModule,
     GastoModule,
     IngresoModule,
-    AuthModule
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
