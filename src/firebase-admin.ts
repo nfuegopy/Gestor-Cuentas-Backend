a90fa03e659d13f1ac427687e67f005bdb3d8b25
Antonio Barrios <93525722+nfuegopy@users.noreply.github.com>
2024-06-09 09:12:36 -0400

    Update firebase-admin.ts
    mod de firebaseadmin

--- src/firebase-admin.ts
+++ src/firebase-admin.ts
@@ -1,11 +1,36 @@
 import * as admin from 'firebase-admin';
-import { ServiceAccount } from 'firebase-admin';
 
-const serviceAccount: ServiceAccount = require('../src/config/serviceAccountKey.json');
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     databaseURL: 'https://gastos-16b0e.firebaseio.com',
 });
 
 export default admin;
+
+
+
+
+//import * as admin from 'firebase-admin';
+//import { ServiceAccount } from 'firebase-admin';
+
+//const serviceAccount: ServiceAccount = require('../src/config/serviceAccountKey.json');
+
+//admin.initializeApp({
+//  credential: admin.credential.cert(serviceAccount),
+// databaseURL: 'https://gastos-16b0e.firebaseio.com',
+//});
+
+//export default admin;
